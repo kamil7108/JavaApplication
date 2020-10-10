9302fb8905ba85e00419e70147bf3f7445d6d0e5
Kamil Machulik <kamilmachulik7108@gmail.com>
2020-10-10 12:48:22 +0200

    Init Project

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -5,6 +5,6 @@ package io.github.mat3e;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Czesc");
     }
 }
